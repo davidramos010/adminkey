ef569afd6425caacc424d7164ff7893fe08addc6
davidramos010 <davidfernandoramos010@gmail.com>
2022-08-17 22:32:13 +0200

    Ajustes en impresion de llave

--- config/params.php
+++ config/params.php
@@ -4,4 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'contacto' => 'Empresa Reparaciones SL - Telf. 123456789 - Palamos',
 ];
