c77237980e984bcbcbfa8894656d3a613f0f1acc
davidramos010 <davidfernandoramos010@gmail.com>
2022-09-11 22:53:56 +0200

    Ajustes en iconos

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -44,8 +44,8 @@ $strUserName = (!empty(Yii::$app->user) && isset(Yii::$app->user) && isset(Yii::
                         'visible' => ((int) Yii::$app->user->identity->perfiluser->id_perfil==1)
                     ],
                     ['label' => 'Registro',  'icon' => 'fas fa-edit', 'url' => ['registro/create']],
-                    ['label' => 'Reporte',  'icon' => 'fas fa-edit', 'url' => ['registro/index']],
-                    /*['label' => 'Contratos',  'icon' => 'file-code', 'url' => ['contratos/generar-list']],*/
+                    ['label' => 'Reporte',  'icon' => 'fas fa-columns', 'url' => ['registro/index']],
+                    ['label' => 'Contratos',  'icon' => 'fas fa-file-contract', 'url' => ['contratos/generar-list']],
                     ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
                     /*['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
                     ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],*/
